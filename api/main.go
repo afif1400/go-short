5ceb0e4096fbbc31cb910d68997d9a4c94bfe15d
afif1400 <afifahmed456123@gmail.com>
2024-02-23 02:59:42 +0530

    fix: autoload .env if exists

--- api/main.go
+++ api/main.go
@@ -1,28 +1,21 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"github.com/afif1400/urlshortner/routes"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv/autoload"
+	"log"
+	"os"
 )
 
 func setupRoutes(app *fiber.App) {
+	app.Get("/", routes.Home)
 	app.Get("/:url", routes.ResolveURL)
 	app.Post("/api/v1", routes.ShortenURL)
 }
 
 func main() {
-
-	if os.Getenv("ENV") == "development" {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Error loading .env file")
-		}
-	}
 
 	app := fiber.New()
 	app.Use(logger.New())
